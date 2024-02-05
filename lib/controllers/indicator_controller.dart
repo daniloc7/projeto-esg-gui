be81d4a7a490c82a9b463d121463d4c4c5b74860
Danilo Christensen <danilocarneiro7@gmail.com>
2024-02-05 16:25:49 -0300

    feat: new function getTotalIndicators

--- lib/controllers/indicator_controller.dart
+++ lib/controllers/indicator_controller.dart
@@ -1,29 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:projeto/interfaces/database_interface.dart';
+import 'package:projeto/models/indicator_model.dart';
 
 class IndicatorController implements DatabaseInterface {
+  List<IndicatorModel> indicatorList = [];
+  FirebaseFirestore databaseReference = FirebaseFirestore.instance;
   @override
-  Future<bool> add(String collection, List<Map<String, dynamic>> data) {
-    // TODO: implement add
-    throw UnimplementedError();
+  Future<bool> add(String collection, List<Map<String, dynamic>> data) async {
+    try {
+      for (var item in data) {
+        await databaseReference.collection(collection).add(item);
+        print('length data' + data.length.toString());
+      }
+      return true;
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
   }
 
   @override
   Future<bool> addOne(String collection, Map<String, dynamic> data,
-      {String? parent}) {
-    // TODO: implement addOne
-    throw UnimplementedError();
+      {String? parent}) async {
+    try {
+      await databaseReference
+          .collection(collection)
+          .add(data)
+          .then((DocumentReference doc) async {
+        await databaseReference
+            .collection(collection)
+            .doc(doc.id)
+            .update({"id": doc.id});
+      });
+      return true;
+    } catch (e) {
+      print(e.toString());
+      return false;
+    }
   }
 
   @override
-  Future<bool> delete(String collection, String doc, String id) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<bool> delete(String collection, String doc, String id) async {
+    try {
+      await databaseReference.collection(collection).doc(doc).delete();
+      print("documento deletado");
+      return true;
+    } catch (e) {
+      print("Erro ao deletar: $e");
+      return false;
+    }
   }
 
   @override
   Future getAll(String collection) {
-    // TODO: implement getAll
-    throw UnimplementedError();
+    return FirebaseFirestore.instance.collection(collection).get().then(
+      (querySnapshot) {
+        for (var docSnapshot in querySnapshot.docs) {
+          indicatorList.add(IndicatorModel.fromJson(docSnapshot.data()));
+        }
+      },
+      onError: (e) => print("Erro para buscar os elementos: $e"),
+    );
   }
 
   @override
@@ -33,8 +70,29 @@ class IndicatorController implements DatabaseInterface {
   }
 
   @override
-  Future getOne(String collection, String doc) {
-    // TODO: implement getOne
-    throw UnimplementedError();
+  Future getOne(String collection, String doc) async {
+    try {
+      return FirebaseFirestore.instance.collection(collection).doc(doc).get();
+    } catch (e) {
+      print(
+        e.toString(),
+      );
+    }
+  }
+
+  Future<int> getTotalIndicators(String fkIdFactor) async {
+    // List<IndicatorModel> _indicatorsFk = [];
+    int total = 0;
+    try {
+      for (var item in indicatorList) {
+        if (item.fkIdFactor == fkIdFactor) {
+          total += 1;
+        }
+      }
+      print(total);
+    } catch (e) {
+      print(e.toString());
+    }
+    return total;
   }
 }
